431289a0695061ad89804646137766ed2843aa35
jykim <kimorkim@gmail.com>
2018-07-02 16:04:32 +0900

    change port

--- src/utils/chatProxy.js
+++ src/utils/chatProxy.js
@@ -34,7 +34,6 @@ export default class ChatProxy extends EventEmitter {
         // eslint-disable-next-line
         this.peer = new Peer(username, {
             host: window.location.hostname,
-            port: 3000,
             path: '/chat',
         });
         this.peer.on('open', (userId) => {
