b28cd8b367cb625c2e2dcda4688f55a20a96c587
jykim <kimorkim@gmail.com>
2018-07-04 19:16:39 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ChatFeed, Message } from 'react-chat-ui'
+import { ChatFeed, Message } from 'react-chat-ui';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,12 @@ class App extends Component {
         users: [],
         username: 'default' + Math.ceil(Math.random() * 10000),
         is_typing: false,
-        messages: [
-            // new Message({
-            //     id: 1,
-            //     message: "I'm the recipient! (The person you're talking to)",
-            // }), // Gray bubble
-            // new Message({ id: 0, message: "I'm you -- the blue bubble!" }), // Blue bubble
-        ],
-    }
+        messages: [],
+    };
     componentDidMount() {
         const { ChatProxy } = this.props;
         const { username } = this.state;
         this.chatProxy = ChatProxy;
-        console.log(username);
         this.chatProxy.connect(username);
         this.chatProxy.onMessage(this.addMessage);
         this.chatProxy.onUserConnected(this.userConnected);
@@ -29,34 +22,40 @@ class App extends Component {
         console.log('addMessage', content, author);
         this.setState(({ messages, username }) => {
             return {
-                messages: [...messages, {
-                    id: 0,
-                    message: content,
-                    senderName : author,
-                }]
-            }
+                messages: [
+                    ...messages,
+                    {
+                        id: 1,
+                        message: content,
+                        senderName: author,
+                    },
+                ],
+            };
         });
-    }
+    };
     userConnected = () => {
         console.log('userConnected');
-    }
+    };
     userDisconnected = () => {
         console.log('userDisconnected');
-    }
+    };
     send = () => {
         const { input = {} } = this.refs;
         const { value = '...' } = input;
         this.chatProxy.broadcast(value);
         this.setState(({ messages, username }) => {
             return {
-                messages: [...messages, {
-                    id: 1,
-                    message: value,
-                    senderName : username,
-                }]
-            }
+                messages: [
+                    ...messages,
+                    {
+                        id: 0,
+                        message: value,
+                        senderName: username,
+                    },
+                ],
+            };
         });
-    }
+    };
     render() {
         const { messages } = this.state;
         return (
@@ -66,11 +65,9 @@ class App extends Component {
                     isTyping={this.state.is_typing}
                     hasInputField={false}
                     showSenderName={true}
-                     />
-                <input ref='input' />
-                <button onClick={this.send}>
-                    send
-                </button>
+                />
+                <input ref="input" />
+                <button onClick={this.send}>send</button>
             </div>
         );
     }
