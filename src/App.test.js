559ca987a630527af8e690a8265e758bc7431117
JY Kim <kimorkim@gmail.com>
2018-07-07 00:55:50 +0900

    Test (#1)
    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ChatProxy from './utils/chatProxy';
+
 
 it('renders without crashing', () => {
+  const proxy = new ChatProxy();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App ChatProxy={proxy} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
