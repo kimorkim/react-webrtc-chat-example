c8bc40786fd7687f7d27619e58850a6953ae51f8
kimorkim <kimorkim@gmail.com>
2018-07-07 00:55:23 +0900

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import ChatProxy from './utils/chatProxy';
-console.log(ChatProxy);
 const proxy = new ChatProxy();
 ReactDOM.render(<App ChatProxy={proxy} />, document.getElementById('root'));
 registerServiceWorker();
